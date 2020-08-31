38391d6a3428f5dc604694b9c9bafd53de581a83
Praveen Chandran <pc-mac@Praveens-MacBook-Air.local>
2020-08-31 13:34:54 +0400

    collection items added

--- src/App.js
+++ src/App.js
@@ -3,18 +3,15 @@ import {BrowserRouter} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import {Route,Switch} from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
 
-const HatsPage= ()=>(
-  <div>
-    hats page
-  </div>
-)
+ 
 
 function App() {
   return (
     <Switch>
       <Route exact path='/' component={HomePage}></Route>
-      <Route path='/shop/hats' component={HatsPage}></Route>
+      <Route path='/shop' component={ShopPage}></Route>
     </Switch>
      
      
